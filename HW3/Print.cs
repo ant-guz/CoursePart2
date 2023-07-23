bab46dca1263d7ba6956f8b1aee893df4f5da175
ant-guz <anton.guz87@gmail.com>
2023-07-23 23:46:19 +0300

    Done homework3

---
+++ HW3/Print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    public class Print
+    {
+        public void PrintGreenMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+        public void PrintOrangeMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(message);
+        }
+            
+    }
+}
---
+++ HW3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HW3
+{
+    internal class Program
+    {
+        delegate void PrintString(string message);
+        static void Main(string[] args)
+        {
+            PrintString printString;
+            Print print = new Print();
+            printString = print.PrintGreenMessage;
+            printString += print.PrintOrangeMessage;
+
+            printString("I have the different colors.");
+            //set default text color
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
