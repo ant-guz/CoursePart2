a144762f983c7237a4550a7c51904042fed36376
ant-guz <anton.guz87@gmail.com>
2023-08-03 13:28:01 +0300

    Remove local changes

---
+++ HW4/Model/Hobby.cs
@@ -0,0 +1,9 @@
+﻿namespace HW4.Model
+{
+    public class Hobby
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ HW4/Pages/MyPages/AboutMe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HW4.Pages.MyPages
+{
+    public class AboutMeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
